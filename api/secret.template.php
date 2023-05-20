0e6bc4384ee54f32fc01d528c2eddbc25eaa0c47
Shahab Movahhedi <45717968+movahhedi@users.noreply.github.com>
2023-05-20 21:29:00 +0330

    Env & Secret Templates

---
+++ api/secret.template.php
@@ -0,0 +1,9 @@
+<?php
+
+define("IsDebugMode", true);
+define("IsLocalDevMode", true);
+
+define("MATTERMOST_API_URL", "https://mattermost.example.com/api/v4/posts");
+define("MATTERMOST_BOT_TOKEN", "");
+define("MATTERMOST_CHANNEL_ID", "");
+// define("MATTERMOST_CHANNEL_ID", "");
