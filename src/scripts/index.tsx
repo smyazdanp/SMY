209fc8bffbd149031e1822f1712c8470ecb0b4c0
mhdibaei <mhdibaei@outlook.com>
2023-05-22 16:29:37 +0330

    form responsive and some minor changes

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -52,16 +52,16 @@ const swiper = new Swiper(".swiper", {
 const contactUsFormCon = document.getElementById("contactUsFormCon");
 
 const name_input = (
-		<input type="text" name="Name" maxLength={128} placeholder="علی محمدی" onBlur={ValidateName} />
+		<input type="text" name="Name" maxLength={128} placeholder="مثال: علی محمدی" onBlur={ValidateName} />
 	) as HTMLInputElement,
 	mobile_input = (
-		<input type="text" name="Mobile" maxLength={14} placeholder="09123456789" onBlur={ValidateMobile} />
+		<input type="text" name="Mobile" maxLength={14} placeholder="مثال: 09123456789" onBlur={ValidateMobile} />
 	) as HTMLInputElement,
 	email_input = (
-		<input type="text" name="Email" maxLength={128} placeholder="name@example.com" onBlur={ValidateEmail} />
+		<input type="text" name="Email" maxLength={128} placeholder="مثال: name@example.com" onBlur={ValidateEmail} />
 	) as HTMLInputElement,
 	title_input = (
-		<input type="text" name="Title" maxLength={128} placeholder="فروشگاهی" onBlur={ValidateTitle} />
+		<input type="text" name="Title" maxLength={128} placeholder="مثال: فروشگاهی" onBlur={ValidateTitle} />
 	) as HTMLInputElement,
 	text_input = <textarea name="Text" maxLength={1024} placeholder="حداکثر 4 خط"></textarea>,
 	name_alert = <span class="formError"></span>,
